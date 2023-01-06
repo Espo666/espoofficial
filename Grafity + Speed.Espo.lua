608b98f8a3df17b543b61572495fd76d12a4a416
Espo666 <119431491+Espo666@users.noreply.github.com>
2023-01-07 02:52:28 +0700

    Add files via upload

---
+++ Grafity + Speed.Espo.lua
@@ -0,0 +1,335 @@
+gg.setVisible(false)
+
+
+TG =
+gg.makeRequest(" ").content
+if not TG then
+end
+
+
+gg.setVisible(false)
+modbyfami = os.date ('   \
+\
+           ❦ ════ •⊰❂ - ❂⊱• ════ ❦\
+\
+📆Dᴀᴛᴇ📆 : %Y/%m/%d\
+\
+🕛Tɪᴍᴇ🕛 : %H:%M:%S\
+\
+⏳Exᴘɪʀᴇᴅ Sᴄʀɪᴘᴛ⏳ : 2023/1/20\
+\
+           ❦ ════ •⊰❂ - ❂⊱• ════ ❦')
+gg.alert(modbyfami)
+gg.setVisible(true)
+
+
+
+
+
+
+
+if os.date("%Y%m%d") >= "2023120"then 
+gg.alert("EXPIRED")
+os.exit() end
+
+
+Password = ""
+Result = gg.prompt({"🔒 Maskukin Pass"},nil,{"text"})
+if Result[1] == Email and Result[2] == Password then
+gg.toast('ON ✅')
+end
+
+
+
+gg.setVisible(false)
+gg.sleep(500)
+gg.toast(" Loading Data 📂")
+gg.sleep(500)
+gg.toast(" 1 ")
+gg.sleep(500)
+gg.toast(" 2 ")
+gg.sleep(500)
+gg.toast(" 3 ")
+gg.sleep(500)
+gg.toast(" 4 ")
+gg.sleep(500)
+gg.toast(" 5 ")
+gg.sleep(500)
+gg.toast(" 6 ")
+gg.sleep(500)
+gg.toast(" 7 ")
+gg.sleep(500)
+gg.toast(" 8 ")
+gg.sleep(500)
+gg.toast(" 9 ")
+gg.sleep(500)
+gg.toast(" 10 ")
+gg.sleep(500)
+gg.toast("𝐏𝐫𝐞𝐬𝐬 𝐆𝐆 𝐭𝐨 𝐜𝐨𝐧𝐭𝐢𝐧𝐮𝐞")
+
+
+
+
+
+
+function bersihkan()
+    gg.clearResults()
+    gg.clearList()
+end
+function pass()
+  print("╔══╗╔╦╗╔══╗╔══╗╔═╗╔═╗╔══╗╔══╗╔═╗")
+    print("║══╣║║║║╔╗║║══╣║╔╝║╬║╚║║╝║╔╗║║╦╝")
+    print("╠══║║║║║╔╗║╠══║║╚╗║╗╣╔║║╗║╔╗║║╩╗")
+    print("╚══╝╚═╝╚══╝╚══╝╚═╝╚╩╝╚══╝╚══╝╚═╝")
+    print("────────────────────────────────")
+    print("╲╲╭━━━━╮")
+    print("╭╮┃▆┈┈▆┃╭╮")
+    print("┃╰┫▽▽▽┣╯┃")
+    print("╰━┫△△△┣━╯")
+    print("╰━┫△△△┣━╯")
+    print("╲╲┃┈┈┈┈┃")
+    print("╲╲┃┈┏┓┈┃")
+    print("▔▔╰━╯╰━╯")
+gg.toast("⚠️ CENCEL ⚠️")
+end
+
+
+function racemode474()
+  showrm=true
+  while (true) do
+    if gg.isVisible()==true then
+      gg.setVisible(false)
+      showrm=true
+    end
+    if showrm==true then
+      rmmenu=gg.choice({"╔═.✵.═══  ModMenu  ═══════╗"," ╠ GRAVITY + SPEED "," ╠ GRAVITY ONLY "," ╠ 𝐄𝐗𝐈𝐓 ⌫","╚═══════ 𝐁𝐲 : ESPO ═══.✵.═╝"},nil,"                 ❦ ════ •⊰❂ - ❂⊱• ════ ❦      \n\n        🎀 𝐌𝐄𝐍𝐔 𝐕𝐕𝐈𝐏 🎀\n\n ╠ 👥 OWNER : ESPO \n\n ╠ 📜 EXPIRED DATE : 20-1-2023 \n\n ╠ ☎️ INFORMASI : 📞 0858-7907-0950 \n\n                ❦ ════ •⊰❂ - ❂⊱• ════ ❦")
+
+         
+         
+        
+ gg.toast("\n⚠️𝐒𝐜𝐫𝐢𝐩𝐭 𝐛𝐲 : ESPO⚠️\n \n𝚂𝚄𝙱𝚂𝙲𝚁𝙸𝙱𝙴 𝙵𝙾𝚁 𝙼𝙾𝚁𝙴 𝚂𝙲𝚁𝙸𝙿𝚃 𝙷𝙰𝙲𝙺 𝚄𝙿𝙳𝙰𝚃𝙴𝚂")
+         
+
+      if rmmenu==2 then
+        cpm474gravityauto()
+      elseif rmmenu==3 then
+        cpm474gravityglitch()
+      elseif rmmenu==4 then
+        pass()--back to func
+        os.exit()
+      else
+        pass()
+      end
+      showrm=false
+    end
+end
+end
+
+function cpm474gravityglitch()
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    local layarglitch = gg.isVisible
+    local setlayarglitch = gg.setVisible
+    local tampilkanmenuglitch = false
+    local searglitch=true
+    
+    
+    function lupgrafglitch(t)
+    datjj=gg.getListItems()
+    ::tololglitch:: do
+      bglitch=gg.prompt({"Gravity [1;21]","repeat scan","exit"},{[1]=7},{"number","checkbox","checkbox"})
+      if bglitch ~= nil then
+        if bglitch[2]==true and bglitch[3] == true then
+          gg.toast("(⊙.☉) i dont understand")
+          goto tololglitch
+          --cpm474()
+        end   
+        if bglitch[3]~=true then
+            if bglitch[2]~=true then
+              if bglitch[1] ~= "" then
+                local tabb={"-1063256064","-1065353216","-1069547520","-1073741824","-1082130432","0","1065353216","1073741824","1077936128","1082130432","1084227584","1086324736","1088421888","1090519040","1091567616","1092616192","1093664768","1094713344","1095761920","1096810496","1097859072"}
+                local tabbx=bglitch[1]
+                local tabby=tabb[tonumber(tabbx)]
+                for i, idat in ipairs(datjj) do
+                  datjj[i].value=tabby
+                end
+                gg.setValues(datjj)
+                gg.addListItems(datjj)
+                condtime="🔥 PRESS GG LOGO TO NORAMAL 🔥"
+                gg.toast(condtime)
+              else
+                condtime=" 🔥 EDIT VALUE REQUIRED 🔥"
+                gg.toast(condtime)
+              end
+            else --newscan
+              t=nil
+              searglitch=true
+              condtime=" 🔥 PRESS GG LOGO TO NEW SCAN 🔥"
+              gg.toast(condtime)
+            end
+          else--exit
+            brekglitch=true
+          end
+      else
+        pass()
+      end--notnil
+      tampilkanmenuglitch=false
+    end--tolol
+    end--lup
+    
+    function carigrafitasiglitch()
+      gg.setRanges(gg.REGION_ANONYMOUS)--HERE
+      gg.searchNumber("17,170,432;0;0;1,065,353,216;-1:65", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+      gg.refineNumber("1065353216",gg.TYPE_DWORD)
+      t=gg.getResults(99999)
+      gg.addListItems(t)
+      searglitch=false
+      tampilkanmenuglitch=false
+    end--carigrafitasi
+    
+    --mulai program
+    --kondisi Menu(ditampilkan atau tidak)
+    brekglitch=false
+    t=nil
+
+    gg.toast("🔥 PRESS GG LOGO 🔥")
+    while brekglitch==false do
+      if layarglitch(true) then
+        setlayarglitch(false)
+        tampilkanmenuglitch = true
+      end
+      if tampilkanmenuglitch == true then
+        if searglitch==false then
+          lupgrafglitch(t)
+        else
+          bersihkan()
+          carigrafitasiglitch()
+          lupgrafglitch(t)
+        end
+      end
+    end
+end
+
+
+
+-- (step 2)
+function cpm474gravityauto()
+    gg.toast(" 🔥 PRESS GG LOGO TO SCAN 🔥 ")
+    gg.setRanges(gg.REGION_ANONYMOUS)
+    local autolayar = gg.isVisible
+    local autosetautolayar = gg.setVisible
+    local tampilkanmenu = false
+    local s=gg.sleep
+    local sear=true
+    local hasrace=false
+    
+    
+    function lupgraf()
+      datrm=gg.getListItems()
+      if hasrace==true then
+        hasrace=false
+        --SET DEFAULT ENGINE
+        for i, idat in ipairs(datrm) do
+          if datrm[i].value == "1232348144" then
+            datrm[i].value="1143930880"--speed
+          elseif datrm[i].value == grafback then
+            datrm[i].value="1065353216"--gravity
+          end
+        end
+        gg.addListItems(datrm)
+        gg.setValues(datrm)
+        gg.toast(" 🔥 PRESS GG LOGO TO ACTIV 🔥 ")
+        tampilkanmenu=false
+      else
+        gg.processResume()
+        ::tolol:: do
+          b=gg.prompt({"Gravity [1;15]","repeat scan","exit"},{[1]=5},{"number","checkbox","checkbox"})
+          if b ~= nil then
+            if b[2]==true and b[3] == true then
+              gg.toast("(⊙.☉) i dont understand")
+              goto tolol
+            end   
+            if b[3]~=true then
+                if b[2]~=true then
+                  if b[1] ~= "" then
+                    local tabb={"-1059061760","-1073741824","-1093874483","0","1065353216","1073741824","1077936128","1082130432","1084227584","1086324736","1088421888","1090519040","1091567616","1092616192","1094713344"}
+                    local tabbx=b[1]
+                    local tabby=tabb[tonumber(tabbx)]
+                    for i, idat in ipairs(datrm) do
+                      if datrm[i].value == "1143930880" then
+                        datrm[i].value="1232348144"--speed
+                      else
+                        datrm[i].value=tabby--gravity
+                      end
+                    end
+                    
+                    gg.addListItems(datrm)
+                    gg.setValues(datrm)
+                    hasrace=true
+                    condtime=" Press gg To Normal "
+                    gg.toast(condtime)
+                    grafback=tabby
+                  else
+                    condtime="Edit Value Required"
+                    gg.toast(condtime)
+                  end
+                else --newscan
+                  datrm=nil
+                  sear=true
+                  condtime="🔥 PRESS GG LOGO TO NEW SCAN 🔥"
+                  gg.toast(condtime)
+                end
+              else--exit
+                brek=true
+              end
+          else
+            pass()
+          end--notnil
+          tampilkanmenu=false
+        end--tolol
+      end--hasrace
+    end--lup
+    
+    function carigrafitasi()
+      gg.setRanges(gg.REGION_ANONYMOUS)
+      gg.searchNumber("17,170,432;0;0;1,065,353,216;-1:65", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+      gg.refineNumber("1065353216",gg.TYPE_DWORD)
+      t=gg.getResults(99999)
+      
+      gg.addListItems(t)
+      gg.clearResults()
+      
+      gg.searchNumber("1143930880;1174011904;65536;1041865114;0;1048576000;1008981770;1053609165:37", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+      gg.refineNumber("1143930880", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1, 0)
+      tt=gg.getResults(99999)
+      gg.addListItems(tt)
+      sear=false
+      tampilkanmenu=false  
+    end--carigrafitasi
+    
+   
+    --mulai program
+    --kondisi Menu(ditampilkan atau tidak)
+    brek=false
+    t=nil
+    grafback="1065353216"
+    while brek==false do
+      if autolayar(true) then
+        autosetautolayar(false)
+        tampilkanmenu = true
+      end
+      if tampilkanmenu == true then
+        if sear==false then
+          lupgraf()
+        else
+          bersihkan()
+          carigrafitasi()
+        end
+      end
+    end
+  end
+
+-- (step 1)
+
+
+--program start-up
+racemode474()
